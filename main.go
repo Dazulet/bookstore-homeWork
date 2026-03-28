655a5fa7fc5d7eac35eb51a52b3537e41e773597
Daulet <baltashdaulet3@gmail.com>
2026-03-28 09:48:11 +0500

    .

--- main.go
+++ main.go
@@ -12,17 +12,14 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	// books
 	r.HandleFunc("/books", handlers.GetBook).Methods("GET")
 	r.HandleFunc("/books", handlers.AddBook).Methods("POST")
 	r.HandleFunc("/books/{id}", handlers.UpdateBook).Methods("PUT")
 	r.HandleFunc("/books/{id}", handlers.DeleteBook).Methods("DELETE")
 
-	// authors
 	r.HandleFunc("/authors", handlers.GetAuthors).Methods("GET")
 	r.HandleFunc("/authors", handlers.AddAuthor).Methods("POST")
 
-	// categories
 	r.HandleFunc("/categories", handlers.GetCategory).Methods("GET")
 	r.HandleFunc("/categories", handlers.AddCategory).Methods("POST")
 
